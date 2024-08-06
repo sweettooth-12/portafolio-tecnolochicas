3893f01669a4071d15102d7137e23bcac4f0be9b
sweettooth-12 <103901003+sweettooth-12@users.noreply.github.com>
2024-08-06 00:29:05 -0600

    Audio y copy

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ typewriternombre
   .typeString('<span style="color: #27ae60;">Data Scientist Jr</span>')
   .pauseFor(1000)
   .start();
+
+  let audioElement; //Variable para almacenar el objeto de Audio
+
+  const mainElement = document.querySelector('main'); //Seleccionar la etiqueta <main>
+
+  mainElement.addEventListener('click', function () {
+    if (!audioElement) {
+      audioElement = new Audio('assets/musica/rey.mp3');
+      audioElement.volume = 0.1;
+    }
+    audioElement.play();
+  });
+
+  const pauseButton = document.getElementById('pauseButton');
+
+  pauseButton.addEventListener('click', function (){
+    if (audioElement.paused) {
+      audioElement.play();
+      pauseButton.textContent = "Pausar";
+    } else {
+      audioElement.pause();
+      pauseButton.textContent = "Reanudar";
+    }
+  });
+  
+async function copiarAlPortapapeles(texto) {
+  try {
+    await navigator.clipboard.writeText(texto);
+    alert('Texto copiado al portapapeles');
+  } catch (err) {
+    console.error('Error al copiar', err);
+  }
+}
